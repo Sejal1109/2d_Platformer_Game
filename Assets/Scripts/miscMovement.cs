12e7468588a81de68757aed7847dd1cc7bd0793c
Sejal1109 <sejal.shingal2001@gmail.com>
2023-03-03 22:06:19 -0500

    fixed enemy flip

--- Assets/Scripts/miscMovement.cs
+++ Assets/Scripts/miscMovement.cs
@@ -1,13 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class miscMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
+    private float min;
+    private float max;
     [SerializeField] private Vector3[] positions;
+    private bool isFacingRight = true;
     private int index;
 
+    void Start() 
+    {
+        List<float> xpos = new List<float>();
+        foreach(Vector3 x in positions)
+        {
+            xpos.Add(x[0]);
+        }
+        min = xpos.Min();
+        max = xpos.Max();
+    }
     // Update is called once per frame
     void Update()
     {
@@ -23,8 +37,28 @@ public class miscMovement : MonoBehaviour
                 index++;
             }
         }
+
+        if (min == max)
+        {
+            isFacingRight = !isFacingRight;
+        }
+        else if (transform.position.x == min && !isFacingRight)
+        {
+            Flip();
+        }
+        else if (transform.position.x == max && isFacingRight)
+        {
+            Flip();
+        }
     }
 
-    
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
 }
 
